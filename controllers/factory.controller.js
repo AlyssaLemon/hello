442a3b9abece1aeb9516d09e1ccbb4f060e390df
AlyssaLemon <alyssa.jane.lemon@gmail.com>
2018-10-14 16:33:20 -0400

    Update controllers/factory.controller.js
    added additional param to CRUD actions (next) to define next

--- controllers/factory.controller.js
+++ controllers/factory.controller.js
@@ -5,7 +5,7 @@ exports.test = function(req, res) {
   res.send("Greetings from the Test controller!");
 };
 
-exports.factory_create = function(req, res) {
+exports.factory_create = function(req, res, next) {
   var factory = new Factory({
     name: req.body.name
   });
@@ -18,14 +18,14 @@ exports.factory_create = function(req, res) {
   });
 };
 
-exports.factory_details = function(req, res) {
+exports.factory_details = function(req, res, next) {
   Factory.findById(req.params.id, function(err, factory) {
     if (err) return next(err);
     res.send(factory);
   });
 };
 
-exports.factory_update = function(req, res) {
+exports.factory_update = function(req, res, next) {
   Factory.findByIdAndUpdate(req.params.id, { $set: req.body }, function(
     err,
     factory
@@ -35,7 +35,7 @@ exports.factory_update = function(req, res) {
   });
 };
 
-exports.factory_delete = function(req, res) {
+exports.factory_delete = function(req, res, next) {
   Factory.findByIdAndRemove(req.params.id, function(err) {
     if (err) return next(err);
     res.send("Deleted successfully!");
