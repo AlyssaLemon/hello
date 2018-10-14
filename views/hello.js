7df5baf75d5dc18f34bb00d907e9daa1a2c18d5f
AlyssaLemon <alyssa.jane.lemon@gmail.com>
2018-10-14 16:59:31 -0400

    Trying to move some things around

--- views/hello.js
+++ views/hello.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var factory = require("./routes/factory.route.js");
+var factory = require("../routes/factory.route.js");
 var app = express();
 
 var mongoose = require("mongoose");
@@ -23,6 +23,8 @@ app.listen(port, () => {
   console.log("Server is up and running on port numner " + port);
 });
 
+// THIS IS WHY I NEED TO BRUSH UP ON REACT!
+
 // CREATING A FACTORY
 
 function factoryName() {
@@ -34,60 +36,52 @@ function factoryName() {
   var facName = document.getElementById("cfn").value;
   nameSpan.innerHTML = facName;
 
-  // //HERE IS THE FACTORY THAT CREATES THE CHILDREN.
+  //HERE IS THE FACTORY THAT CREATES THE CHILDREN.
 
-  // var howMany = prompt(
-  //   "How many child nodes would you like? You may choose between 1 and 15."
-  // );
-  // if (howMany < 1 || howMany > 15) {
-  //   var howMany = prompt("Please pick a number between 1 and 15.");
-  //   if (howMany < 1 || howMany > 15) {
-  //     alert("You don't get to play anymore!");
-  //   }
-  // } else if (howMany > 0 && howMany < 16) {
-  //   console.log(howMany);
-  // }
-  // // This asks the user to pick two numbers as a range they'd like random numbers to be generated from and assigns them to variables
-  // var lowNum = prompt(
-  //   "Now I will need you to pick a range of numbers. Please pick the lowest number in your range."
-  // );
-  // var highNum = prompt("Please pick the highest number in your range.");
-  // // This generates random numbers within the range provided
-  // const randomFromRange = function(min, max) {
-  //   min = Math.ceil(min);
-  //   max = Math.floor(max);
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // };
-  // console.log(randomFromRange(lowNum, highNum));
-  // // This loops through and creates an array of random numbers.\
-  // let beeps = [];
-  // var i;
-  // for (i = 0; i < howMany; i++) {
-  //   beeps.push(randomFromRange(lowNum, highNum));
-  // }
-  // // Just making sure the array is created properly
-  // console.log(beeps);
-  // // Creates a UL to plop the array items into
-  // let childNodesUL = document.createElement("ul");
-  // childNodesUL.setAttribute("id", "stuff");
-  // document.body.appendChild(childNodesUL);
-  // beeps.forEach(function(itemNum) {
-  //   let li = document.createElement("li");
-  //   let child = document.createTextNode(itemNum);
-  //   li.appendChild(child);
-  //   document.getElementById("stuff").appendChild(li);
-  // });
+  var howMany = prompt(
+    "How many child nodes would you like? You may choose between 1 and 15."
+  );
+  if (howMany < 1 || howMany > 15) {
+    var howMany = prompt("Please pick a number between 1 and 15.");
+    if (howMany < 1 || howMany > 15) {
+      alert("You don't get to play anymore!");
+    }
+  } else if (howMany > 0 && howMany < 16) {
+    console.log(howMany);
+  }
 
-  // Here is the form to create a FACTORY
+  // This asks the user to pick two numbers as a range they'd like random numbers to be generated from and assigns them to variables
+  var lowNum = prompt(
+    "Now I will need you to pick a range of numbers. Please pick the lowest number in your range."
+  );
+  var highNum = prompt("Please pick the highest number in your range.");
 
-  // function factoryName(facName) {
-  //   var newNameDiv = document.createElement("div");
-  //   newNameDiv.setAttribute("class", "newNameDiv");
-  //   document.body.appendChild(newNameDiv);
-  //   var facName = document.getElementById("cfn").value;
-  //   var newNameSpan = document.createElement("span");
-  //   newNameSpan.setAttribute("class", "newNameSpan");
-  //   newNameSpan.appendChild(facName);
-  //   document.body.appendChild(newNameSpan);
-  // }
+  // This generates random numbers within the range provided
+  const randomFromRange = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  console.log(randomFromRange(lowNum, highNum));
+
+  // This loops through and creates an array of random numbers.\
+  let beeps = [];
+  var i;
+  for (i = 0; i < howMany; i++) {
+    beeps.push(randomFromRange(lowNum, highNum));
+  }
+
+  // Just making sure the array is created properly
+  console.log(beeps);
+
+  // Creates a UL to plop the array items into
+  let childNodesUL = document.createElement("ul");
+  childNodesUL.setAttribute("id", "stuff");
+  document.body.appendChild(childNodesUL);
+  beeps.forEach(function(itemNum) {
+    let li = document.createElement("li");
+    let child = document.createTextNode(itemNum);
+    li.appendChild(child);
+    document.getElementById("stuff").appendChild(li);
+  });
 }
